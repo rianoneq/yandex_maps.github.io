397c49eed2924c42a50df907e46d2d4d16de3258
Dmitry <63122696+itRianone@users.noreply.github.com>
2022-12-12 00:17:58 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,39 +1,34 @@
+
 function getCoords() {
+  // function take args from url and create matrix [[x1,y1], [x2,y2]...]
+  let coords = [];
+
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  keys = urlParams.keys(),
-  values = urlParams.values(),
-  entries = urlParams.entries();
-  console.log(keys, values, entries);
+  for (i = 1; i < 10; i++ ) {
+    let coordX = urlParams.get(`x${i}`); 
+    let coordY = urlParams.get(`y${i}`); 
+    if (coordX, coordY) {
+      // '+' - best str to int converter
+      coords.push([+coordX, +coordY]);
+    };
+  };
 
-}
-getCoords()
+  return coords
+};
+
 function init () {
-    /**
-     * Создаем мультимаршрут.
-     * Первым аргументом передаем модель либо объект описания модели.
-     * Вторым аргументом передаем опции отображения мультимаршрута.
-     * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/multiRouter.MultiRoute.xml
-     * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/multiRouter.MultiRouteModel.xml
-     */
     var multiRoute = new ymaps.multiRouter.MultiRoute({
-        // Описание опорных точек мультимаршрута.
-        referencePoints: [
-            [55.734876, 37.59308],
-            [48.682807, 44.465995], 
-            [59.881084, 30.279927]
-        ],
-        // Параметры маршрутизации.
+        referencePoints: getCoords(),
+
+       
         params: {
-            // Ограничение на максимальное количество маршрутов, возвращаемое маршрутизатором.
             results: 2
         }
     }, {
-        // Автоматически устанавливать границы карты так, чтобы маршрут был виден целиком.
         boundsAutoApply: true
     });
 
-    // Создаем кнопки для управления мультимаршрутом.
     var trafficButton = new ymaps.control.Button({
             data: { content: "Учитывать пробки" },
             options: { selectOnClick: true }
@@ -43,12 +38,7 @@ function init () {
             options: { selectOnClick: true }
         });
 
-    // Объявляем обработчики для кнопок.
     trafficButton.events.add('select', function () {
-        /**
-         * Задаем параметры маршрутизации для модели мультимаршрута.
-         * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/multiRouter.MultiRouteModel.xml#setParams
-         */
         multiRoute.model.setParams({ avoidTrafficJams: true }, true);
     });
 
@@ -56,17 +46,11 @@ function init () {
         multiRoute.model.setParams({ avoidTrafficJams: false }, true);
     });
 
-    viaPointButton.events.add('select', function () {
-        var referencePoints = multiRoute.model.getReferencePoints();
-        referencePoints.splice(1, 0, "Москва, ул. Солянка, 7");
-        /**
-         * Добавляем транзитную точку в модель мультимаршрута.
-         * Обратите внимание, что транзитные точки могут находится только
-         * между двумя путевыми точками, т.е. не могут быть крайними точками маршрута.
-         * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/multiRouter.MultiRouteModel.xml#setReferencePoints
-         */
-        multiRoute.model.setReferencePoints(referencePoints, [1]);
-    });
+    // viaPointButton.events.add('select', function () {
+    //     var referencePoints = multiRoute.model.getReferencePoints();
+    //     referencePoints.splice(1, 0, "Москва, ул. Солянка, 7");
+    //     multiRoute.model.setReferencePoints(referencePoints, [1]);
+    // });
 
     viaPointButton.events.add('deselect', function () {
         var referencePoints = multiRoute.model.getReferencePoints();
@@ -76,9 +60,9 @@ function init () {
 
     // Создаем карту с добавленными на нее кнопками.
     var myMap = new ymaps.Map('map', {
-        center: [55.750625, 37.626],
+      center: [55.750625, 37.626],
         zoom: 7,
-        controls: [trafficButton, viaPointButton]
+        //controls: [trafficButton, viaPointButton]
     }, {
         buttonMaxWidth: 300
     });
